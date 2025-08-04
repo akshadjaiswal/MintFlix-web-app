2ad89e569c5b8f00f8f27239fb7d375a89d128da
YourUsername <akshadsantoshjaiswal@gmail.com>
2025-08-04 23:33:24 +0530

    Login Form 🔢

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://analyticsindiamag.com/wp-content/uploads/2019/05/apps.55787.9007199266246365.687a10a8-4c4a-4a47-8ec5-a95f70d8852d.jpg"
+          alt="logo"
+        ></img>
+      </div>
       <div>
-         <img src='https://analyticsindiamag.com/wp-content/uploads/2019/05/apps.55787.9007199266246365.687a10a8-4c4a-4a47-8ec5-a95f70d8852d.jpg' alt='logo'></img>
+
+      <form className=" w-3/12 absolute p-12 bg-black  my-36 mx-auto right-0 left-0 text-white ">
+      <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input type="text" placeholder="Email Address" className="p-2 m-2 " />
+        <input type="password" placeholder="Password" className="p-2 m-2 " />
+        <button className="p-4 m-4 bg-red-700 ">Sign in</button>
+      </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
