0b676f40a05e976187316e4b4a797345a822cf55
YourUsername <akshadsantoshjaiswal@gmail.com>
2025-08-04 23:09:12 +0530

    Header :art: ✅

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div className='text-4xl font-bold'>Login</div>
+    <div>
+      <Header/>
+      <div>
+         <img src='https://analyticsindiamag.com/wp-content/uploads/2019/05/apps.55787.9007199266246365.687a10a8-4c4a-4a47-8ec5-a95f70d8852d.jpg' alt='logo'></img>
+      </div>
+    </div>
   )
 }
 
